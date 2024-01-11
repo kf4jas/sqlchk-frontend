2fa599f6e0a7d1afcc4b71d5a170037727d9e0e1
Joe <joe@unherd.info>
2024-01-11 15:29:28 -0500

    adds the load data portion of the app

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -6,6 +6,9 @@ The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 [Also based on](https://github.com/conventional-changelog/standard-version/blob/master/CHANGELOG.md) so decending.
 
 ## [0.1.2] - 2024-01-11
+### Added
+- adds the load data portion of the app
+
 ### Changed
 - updates headers to reflect the correct locations on the golang app
 
--- src/routes/lookup/out.svelte
+++ src/routes/lookup/out.svelte
@@ -1,32 +1,6 @@
 <script>
     export let sqlOutput;
 </script>
-<style>
-    div{
-        margin: 1rem;
-        box-shadow: 0 2px 8px rgba(0, 0, 0, 0.26)
-    }
-    h1{
-        font-size: 1.25rem;
-        margin: 0.25rem 0;
-    }
-    h2{
-        font-size: 1rem;
-        margin: 0.25rem 0;
-        color: aqua;
-    }
-    p{
-        margin: 0.25rem 0;
-    }
-    button{
-        font : larger;
-        padding: 0.15rem 0.5rem;
-        background-color: #1b1a1a;
-        border: 1px solid  aliceblue ;
-        cursor: pointer;
-        color: white;
-    }
-</style>
 
 <div>
     <pre> {sqlOutput}</pre>
